f08df565e02378880f7a4c4952171933f3176e61
dyerman456 <dyerman456@gmail.com>
2024-05-08 12:39:48 +0200

    bestSolution

--- ANeedleINTheHaystack.js
+++ ANeedleINTheHaystack.js
@@ -1,8 +1,3 @@
 function findNeedle(haystack) {
-    for(let i = 0;i < haystack.length; i++){
-      if(haystack[i] == "needle"){
-        return `found the needle at position ${i}`;
-      }
-    }
-  }
-  
+    return `found the needle at position ${haystack.indexOf("needle")}`;
+}
